2db9a9f6dbbd6f0fb8f5fbaff3526c0364f3a4ee
Dheiksita <88729805+Dheiksita@users.noreply.github.com>
2022-05-09 19:49:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function songs(){
+    window.location="songs.html";
+}
+function karoke(){
+    window.location="karoke.html";
+}
+function rhymes(){
+    window.location="rhymes.html";
+}
+function sounds(){
+    window.location="sound.html";
+}
